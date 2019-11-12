9ebb8d6ef0668f1a8cd798eb7c92504911dfb176
TerryChan2003 <aleksandr_h2001@mail.ru>
2019-11-12 21:01:23 +0300

    deploy

---
+++ precache-manifest.7f1377a3a5503fd6de0ae7389a12b65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5b92d7fc1d6ce3153daa8a5e589f17",
+    "url": "/vk_mini_chat_project/index.html"
+  },
+  {
+    "revision": "904edcae3c69fc4152b4",
+    "url": "/vk_mini_chat_project/static/css/2.0ca46490.chunk.css"
+  },
+  {
+    "revision": "b0364fd111ee7a09d323",
+    "url": "/vk_mini_chat_project/static/css/main.d9e75851.chunk.css"
+  },
+  {
+    "revision": "904edcae3c69fc4152b4",
+    "url": "/vk_mini_chat_project/static/js/2.6419df98.chunk.js"
+  },
+  {
+    "revision": "b0364fd111ee7a09d323",
+    "url": "/vk_mini_chat_project/static/js/main.f2f324f1.chunk.js"
+  },
+  {
+    "revision": "a1a2435440819ab2e5f0",
+    "url": "/vk_mini_chat_project/static/js/runtime-main.c5d1c55d.js"
+  }
+]);
